1ce7a5da0a210bf1a2eace67ab44d00288092334
shamimhcp1 <shamimhcp1@gmail.com>
2024-03-17 13:03:49 +0600

    chapter-14, form validition with useFormState in create-invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -7,32 +7,61 @@ import { redirect } from 'next/navigation';
 
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(['pending', 'paid']),
+  customerId: z.string({
+    invalid_type_error: 'Please select a customer',
+  }),
+  amount: z.coerce.number().gt(0, {
+    message: 'Amount must be greater than 0',
+  }),
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: 'Please select an invoice status',
+  }),
   date: z.string(),
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+}
+
 // create invoice
-export async function createInvoice(formData: FormData) {
+export async function createInvoice(prevState: State, formData: FormData) {
   // If you're working with forms that have many fields, you may want to consider using the `FormData.entries()` method to convert the form data to a key/value object.
   // const rawFormData = Object.fromEntries(formData.entries());
 
   // Alternatively, you can use the `FormData.get()` method to get the value of a specific field.
-  const { customerId, amount, status } = CreateInvoice.parse({
+  const validatedFields = CreateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
 
+  // if form validation fails, return errors early, otherwise continue
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing fields, Failed to Create Invoice.',
+    };
+  }
+  // Prepare data for insertion into the database
+  const { customerId, amount, status } = validatedFields.data;
+
+  // Convert amount to cents
   const amountInCents = amount * 100;
+
+  // Get current date
   const date = new Date().toISOString().split('T')[0];
 
+  // Insert data into the database
   try {
-    await sql `
+    await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
@@ -40,9 +69,10 @@ export async function createInvoice(formData: FormData) {
     console.error('Database Error:', error);
     return {
       message: 'Database Error: Failed to create invoice.',
-    }
+    };
   }
 
+  // revalidate the page
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
