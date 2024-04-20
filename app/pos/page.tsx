4958ca4d04fce8eed5a216d8f5e83d819e02987c
shamimhcp1 <shamimhcp1@gmail.com>
2024-04-20 16:42:41 +0600

    added /pos to the auth

---
+++ app/pos/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>Pos Page</div>
+  )
+}
+
+export default Page
--- auth.config.ts
+++ auth.config.ts
@@ -8,10 +8,12 @@ export const authConfig = {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
-      if (isOnDashboard) {
+      const isOnPos = nextUrl.pathname.startsWith('/pos');
+      if (isOnDashboard || isOnPos) {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
-      } else if (isLoggedIn) {
+      }       
+      else if (isLoggedIn) {
         return Response.redirect(new URL('/dashboard', nextUrl));
       }
       return true;
